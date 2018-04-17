59adbd2165f96fe795357f9e3f250b4d387b868b
xen0f0n <akis.karagian@gmail.com>
2018-04-17 08:49:13 +0300

    added true color img

--- pavia_visualize_array.py
+++ pavia_visualize_array.py
@@ -2,12 +2,22 @@ import scipy.io as sio
 from scipy.misc import imsave
 import numpy as np
 
-mat = sio.loadmat('/home/xen0f0n/Downloads/PaviaU.mat')
+data = sio.loadmat('/home/xen0f0n/hyperspectral_data_classification/PaviaU.mat')
 
-a = np.zeros(shape=(610,340))
+test_img = np.zeros(shape=(610,340))
 for i in range(610):
     for j in range(340):
         # i, j indexes for spatial dimensions, spectral channel 100th
-        a[i][j] = mat['paviaU'][i][j][100]
+        test_img[i][j] = data['paviaU'][i][j][100]
 
-imsave('test.png', a)
+imsave('test_img.png', test_img)
+
+true_color_img = np.zeros(shape=(610,340, 3))
+for i in range(610):
+    for j in range(340):
+        # i, j indexes for spatial dimensions, spectral channel 100th
+        true_color_img[i][j][0] = data['paviaU'][i][j][45] # RED channel
+        true_color_img[i][j][1] = data['paviaU'][i][j][27] # GREEN channel
+        true_color_img[i][j][2] = data['paviaU'][i][j][11] # BLUE channel
+
+imsave('true_color_img.png', true_color_img)
